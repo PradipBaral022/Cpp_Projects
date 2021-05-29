9a63adc52a62e35e62b186e524736be65bf1da91
Pradip Baral <baralpradip8848@gmail.com>
2021-05-29 12:18:55 +0545

    If else statement in c++

---
+++ Assignments/Assignments_2(Statements)/integer.cpp
@@ -0,0 +1,24 @@
+//Program to print positive number entered by the user.
+//Program to check whether an integer is positive or negative
+//Program to check whether an integer is positive, negative or zero
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter any integer:";
+    cin >> n;
+    if (n > 0)
+    {
+        cout << n << " is a positive number!" << endl;
+    }
+    else if (n < 0)
+    {
+        cout << n << " is a negative number!" << endl;
+    }
+    else
+    {
+        cout << "Integer is zero!" << endl;
+    }
+}
