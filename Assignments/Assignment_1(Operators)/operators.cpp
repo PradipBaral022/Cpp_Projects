536766536d5cf927fe51d161e525519d2724b386
Pradip Baral <baralpradip8848@gmail.com>
2021-06-04 15:19:54 +0545

    Operators in C++

--- Assignments/Assignment_1(Operators)/operators.cpp
+++ Assignments/Assignment_1(Operators)/operators.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int main()
 {
     //Assignment and assignment operators:
-    float sum, a, b, product, difference, div;
+    float sum, a, b, prod, difference, div;
     cout << "Enter 1st num:";
     cin >> a;
     cout << "Enter the 3nd num:";
     cin >> b;
     sum = a + b;
-    product = a * b;
+    prod = a * b;
     difference = a - b;
     div = a / b;
     cout << "The sum is:" << sum << endl
          << "The difference is:" << difference << endl
-         << "The multiplication is:" << product << endl
+         << "The multiplication is:" << prod << endl
          << "The division is:" << div << endl;
     //logical and comparison operators
-    if (a > div && product > difference)
+    if (a > div && prod > difference)
     {
         cout << "You are clear in operators" << endl;
     }
