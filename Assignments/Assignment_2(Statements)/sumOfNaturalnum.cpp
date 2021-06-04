919b1ec89f9d0853e22812a76460838ed3b9bc0b
Pradip Baral <baralpradip8848@gmail.com>
2021-06-04 15:12:13 +0545

    .gitignore is now working

---
+++ Assignments/Assignment_2(Statements)/sumOfNaturalnum.cpp
@@ -0,0 +1,17 @@
+//C++ program to find the sum of first n natural numbers
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i = 1;
+    int sum = 0;
+    cout << "Enter any +ve number:";
+    cin >> n;
+    do
+    {
+        sum += i;
+        i++;
+    } while (i <= n);
+    cout << "The sum is:" << sum << endl;
+}
