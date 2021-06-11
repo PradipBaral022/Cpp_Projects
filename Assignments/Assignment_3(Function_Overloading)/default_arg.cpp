9a7e076bd39a81e387266bdb7083296f69f77b96
Pradip Baral <baralpradip8848@gmail.com>
2021-06-11 14:02:29 +0545

    Function Overloading in C++

---
+++ Assignments/Assignment_3(Function_Overloading)/default_arg.cpp
@@ -0,0 +1,34 @@
+// example of function overloading with default argument
+
+#include <iostream>
+
+using namespace std;
+
+int sum(int x, int y, int z)
+
+{
+
+    int sum1 = x + y + z;
+
+    return (sum1);
+}
+
+int sum(int a = 3, int b = 6)
+
+{
+
+    int sum2 = a + b;
+
+    return (sum2);
+}
+
+int main()
+
+{
+
+    cout << sum(2, 5, 4) << endl;
+
+    cout << sum(2, 6) << endl;
+
+    return 0;
+}
